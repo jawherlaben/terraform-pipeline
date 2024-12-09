a639ca9c7a2b07e94c00ea801a069f5619eb88cd
jawherlaben <Jawher.labben@insat.u-carthage.tn>
2024-12-09 19:00:16 +0100

    push to read

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,8 +1,13 @@
-provider "kubernetes" {
-  config_path = "~/.kube/config"
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.11.0"
+    }
+  }
 }
 
 provider "kubernetes" {
   config_path    = "~/.kube/config"
   config_context = "minikube"
-} 
+}
