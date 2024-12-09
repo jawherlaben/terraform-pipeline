f189a2cb1d234125a4343555df1d0db23f504bbc
jawherlaben <Jawher.labben@insat.u-carthage.tn>
2024-12-09 18:52:44 +0100

    delete lines in main file

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = "2.11.0"
-    }
-  }
-}
-
 provider "kubernetes" {
   config_path    = "~/.kube/config"
   config_context = "minikube"
